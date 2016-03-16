8e2888e1decc69c908becf6261c0a6901eac5fca
Jigar Gosar <jigar.gosar@gmail.com>
2016-03-16 16:20:48 +0530

    update

--- setup-git-alias.sh
+++ setup-git-alias.sh
@@ -12,5 +12,6 @@ git config --global --replace-all alias.amend 'commit --amend'
 
 
 git config --global --replace-all alias.precommit 'diff --cached --diff-algorithm=minimal -w'
-git config --global --replace-all alias.graph 'log --graph -10 --branches --remotes --tags  --format=format:''%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %cr) %Cred%d\'' --date-order'
-git config --global --replace-all alias.nevermind '!git reset --hard HEAD   && git clean -dfx'
+git config --global --replace-all alias.graph 'log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)" --all'
+
+git config --global --replace-all alias.nevermind !'git reset --hard HEAD   && git clean -dfx'
